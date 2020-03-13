0a19da892bac0e13940fe411687e0367c0964475
okutamatanuki <seinoazumi@gmail.com>
2020-03-13 23:26:58 +0900

    RSpecの導入（ttetstフォルダの削除）

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+describe "タスク管理機能", type: :system do
+  describe "一覧表示機能" do
+    let!(:user_a) { FactoryBot.create(:user) }
+    let!(:task) { FactoryBot.create(:task, name: "最初のタスク", user: user_a) }
+
+    context "ユーザーAとしてログインしている時" do
+      it "作成したタスクが表示される" do
+        visit login_path
+        fill_in "session[email]", with: user_a.email
+        fill_in "session[password]", with: user_a.password
+        click_button 'ログインする'
+        expect(page).to have_content("最初のタスク")
+      end
+    end
+  end
+end
